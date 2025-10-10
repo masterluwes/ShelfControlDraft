ab8a405009741884c0c937b644a8aa9e6158d666
TheSilverCoder <ldenriquez85@gmail.com>
2025-10-10 21:35:17 +0800

    Loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:shelf_control/screens/create_account_page.dart';
 import 'package:shelf_control/screens/login_page.dart';
 import 'package:shelf_control/screens/guest_page.dart';
+import 'package:shelf_control/screens/splash_screen.dart';
 import 'package:shelf_control/screens/welcome_page.dart';
 import 'package:shelf_control/screens/feature_preview_screen.dart';
 import 'package:shelf_control/screens/dashboard_page.dart'; // Import DashboardPage
@@ -44,30 +45,7 @@ class _ShelfControlAppState extends State<ShelfControlApp> {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator(); // Or a splash screen
-          }
-          if (snapshot.hasData) {
-            // User is logged in, set the selected household
-            final firestoreService =
-                Provider.of<FirestoreService>(context, listen: false);
-            return FutureBuilder<void>(
-              future: firestoreService.setInitialHousehold(snapshot.data!.uid),
-              builder: (context, householdSnapshot) {
-                if (householdSnapshot.connectionState ==
-                    ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-                return const DashboardPage(); // Navigate to DashboardPage
-              },
-            );
-          }
-          return const WelcomePage(); // User is not logged in
-        },
-      ),
+      home: const SplashScreen(),
       routes: {
         '/create-account': (context) => const CreateAccountPage(),
         '/login': (context) => const LoginPage(),
